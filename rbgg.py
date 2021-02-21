05c411d12464f30f6805db8459a7703eb04e9ef9
Wojciech Blachowski <wblachowsky@gmail.com>
2021-02-21 23:45:03 +0100

    Add margins code

--- rbgg.py
+++ rbgg.py
@@ -16,30 +16,48 @@ parser.add_argument('-m', '--mask', action="store_true",
                     help='whether to save masks')
 parser.add_argument('-mo', '--mask-out-dir', default='mask',
                     help='mask output directory')
+parser.add_argument('--margin-low', type=int, help='margin low')
+parser.add_argument('--margin-high', type=int, help='margin high')
+parser.add_argument('--margins-equal', action="store_true",
+                    help='equal margins')
+parser.add_argument('--scale-low', type=float, help='scale low')
+parser.add_argument('--scale-high', type=float, help='scale high')
 
 
-def get_random_cutout(im):
-    y = random.randint(0, im.shape[0]//15)
-    x = random.randint(0, im.shape[1]//15)
-    width = im.shape[1]-x - \
-        random.randint(-(im.shape[1]-y)//15, (im.shape[1]-x)//15)
-    height = im.shape[0]-y - \
-        random.randint(-(im.shape[0]-y)//15, (im.shape[0]-y)//15)
-    cutout = im[y:y+height, x:x+width]
-    scale = min(random.random()+0.2, 1)
-    cutout = cv2.resize(cutout, tuple(int(scale*x)
-                                      for x in im.shape)[:-1][-1::-1])
-    if random.random() > 0.8:
-        blur_rate = max(1, random.randrange(
-            min(cutout.shape[0], cutout.shape[1])//15))
-        cutout = cv2.blur(cutout, (blur_rate, blur_rate))
-    return cutout
+def get_random_cutout(image, scale_low, scale_high, margin_low, margin_high, margins_equal):
+    if scale_low or scale_high:
+        if not scale_low:
+            scale_low = scale_high
+        if not scale_high:
+            scale_high = scale_low
+        scale = random.uniform(scale_low, scale_high)
+        image = cv2.resize(image, (image.shape[1]*scale, image.shape[0]*scale))
+    if margin_low or margin_high:
+        if not margin_low:
+            margin_low = margin_high
+        if not margin_high:
+            margin_high = margin_low
+        if margins_equal:
+            margin = random.randint(margin_low, margin_high)
+            print(margin)
+            print(image.shape)
+            image = np.pad(image, ((margin, margin), (margin, margin), (0, 0)))
+        else:
+            def rndm(): return random.randint(margin_low, margin_high)
+            image = np.pad(image, ((rndm(), rndm()), (rndm(), rndm()), (0, 0)))
+
+    # if random.random() > 0.8:
+        # blur_rate = max(1, random.randrange(
+        # min(cutout.shape[0], cutout.shape[1])//15))
+        # cutout = cv2.blur(cutout, (blur_rate, blur_rate))
+    return image
 
 
 def get_random_solid_background(im):
     random_color = [random.randrange(
         256), random.randrange(256), random.randrange(256)]
-    return np.full((im.shape[0]+random.randrange(-im.shape[0]//4, im.shape[0]//3), im.shape[1]+random.randrange(-im.shape[1]//4, im.shape[1]//3), 3), random_color)
+    return np.full((im.shape[0], im.shape[1], 3), random_color)
+    # return np.full((im.shape[0]+random.randrange(-im.shape[0]//4, im.shape[0]//3), im.shape[1]+random.randrange(-im.shape[1]//4, im.shape[1]//3), 3), random_color)
 
 
 def overlay_transparent(background, overlay, x, y):
@@ -87,11 +105,13 @@ if __name__ == '__main__':
     os.makedirs(args.mask_out_dir, exist_ok=True)
 
     for i in range(args.number):
-        cutout = get_random_cutout(im)
+        print(args.margins_equal)
+        cutout = get_random_cutout(im, args.scale_low, args.scale_high,
+                                   args.margin_low, args.margin_high, args.margins_equal)
         background = get_random_solid_background(cutout)
-        x = random.randrange(max(1, background.shape[1]-cutout.shape[1]))
-        y = random.randrange(max(1, background.shape[0]-cutout.shape[0]))
-        added_image, mask = overlay_transparent(background, cutout, x, y)
+        # x = random.randrange(max(1, background.shape[1]-cutout.shape[1]))
+        # y = random.randrange(max(1, background.shape[0]-cutout.shape[0]))
+        added_image, mask = overlay_transparent(background, cutout, 0, 0)
         added_image = added_image.astype('uint8')
 
         cv2.imwrite(f'{args.out_dir}/{i}.jpg', added_image)
