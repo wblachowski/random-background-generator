4400677f2d013ab2ba9bf422a16875d6299b1431
Wojciech Blachowski <wblachowsky@gmail.com>
2021-02-22 13:08:05 +0100

    Refactor margins to accept floats

--- rbgg.py
+++ rbgg.py
@@ -16,8 +16,8 @@ parser.add_argument('-m', '--mask', action="store_true",
                     help='whether to save masks')
 parser.add_argument('-mo', '--mask-out-dir', default='mask',
                     help='mask output directory')
-parser.add_argument('--margin-low', type=int, help='margin low')
-parser.add_argument('--margin-high', type=int, help='margin high')
+parser.add_argument('--margin-low', type=float, help='margin low')
+parser.add_argument('--margin-high', type=float, help='margin high')
 parser.add_argument('--margins-equal', action="store_true",
                     help='equal margins')
 parser.add_argument('--scale-low', type=float, help='scale low')
@@ -40,19 +40,23 @@ def get_random_cutout(image, scale_low, scale_high, margin_low, margin_high, mar
             scale_high = scale_low
         scale = random.uniform(scale_low, scale_high)
         image = cv2.resize(image, (image.shape[1]*scale, image.shape[0]*scale))
+
     if margin_low or margin_high:
         if not margin_low:
             margin_low = margin_high
         if not margin_high:
             margin_high = margin_low
         if margins_equal:
-            margin = random.randint(margin_low, margin_high)
-            image = np.pad(image, ((max(0, margin), max(0, margin)),
-                                   (max(0, margin), max(0, margin)), (0, 0)))
-            image = unpad(image, ((margin, margin), (margin, margin), (0, 0)))
+            margin = random.unform(margin_low/2, margin_high/2)
+            a, b, c, d = int(margin*image.shape[0]), int(margin*image.shape[0]), int(
+                margin*image.shape[1]), int(margin*image.shape[1])
+            image = np.pad(image, ((max(0, a), max(0, b)),
+                                   (max(0, c), max(0, d)), (0, 0)))
+            image = unpad(image, ((a, b), (c, d), (0, 0)))
         else:
-            def rndm(): return random.randint(margin_low, margin_high)
-            a, b, c, d = rndm(), rndm(), rndm(), rndm()
+            def rndm(): return random.uniform(margin_low/2, margin_high/2)
+            a, b, c, d = int(rndm()*image.shape[0]), int(rndm()*image.shape[0]), int(
+                rndm()*image.shape[1]), int(rndm()*image.shape[1])
             image = np.pad(image, ((max(0, a), max(0, b)),
                                    (max(0, c), max(0, d)), (0, 0)))
             image = unpad(image, ((min(0, a), min(0, b)),
